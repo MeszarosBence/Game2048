c60b27a5ee74d4839259c63cc28e4e9ea07cb41f
Bence Meszaros <bence.meszaros@gmail.com>
2017-08-22 10:59:02 +0200

    Left movements implemented

--- Game2048/src/main/java/bence/game2048/Game2048.java
+++ Game2048/src/main/java/bence/game2048/Game2048.java
@@ -1,9 +1,11 @@
 package bence.game2048;
 
 import java.io.PrintStream;
+import java.util.Random;
 
 public class Game2048 {
 
+	private static final int TABLE_SIZE = 4;
 	private static final String HORIZONTAL = "\u2550";
 	private static final String VERTICAL = "\u2551";
 	private static final String UPPER_LEFT = "\u2554";
@@ -17,19 +19,24 @@ public class Game2048 {
 	private static final String CELL_LEFT = "\u2560";
 	private static final String CELL_RIGHT = "\u2563";
 	private static final String SPACE = " ";
+	
+	private static final int LEFT = 57419;
+	private static final int RIGHT = 57421;
+	private static final int UP = 57416;
+	private static final int DOWN = 57424;
 
 	PrintStream out;
-	int[][] content;
+	int[][] table = new int[TABLE_SIZE][TABLE_SIZE];
 
 	public void printTable() {
 		out.print(topBorderRow());    
-		out.print(contentRow(content[0]));      
+		out.print(contentRow(table[0]));      
 		out.print(middleBorderRow()); 
-		out.print(contentRow(content[1]));      
+		out.print(contentRow(table[1]));      
 		out.print(middleBorderRow()); 
-		out.print(contentRow(content[2]));      
+		out.print(contentRow(table[2]));      
 		out.print(middleBorderRow()); 
-		out.print(contentRow(content[3]));      
+		out.print(contentRow(table[3]));      
 		out.print(bottomBorderRow()); 
 	}
 
@@ -76,4 +83,72 @@ public class Game2048 {
 		return Integer.toString(i);
 	}
 
+	public Cell getNextCell() {
+		Random rand = new Random();
+		return new Cell(2, rand.nextInt(TABLE_SIZE), rand.nextInt(TABLE_SIZE));
+	}
+
+	public void start() {
+		Cell cell = getNextCell();
+		table[cell.getX()][cell.getY()] = cell.getValue();
+		printTable();
+		
+	}
+
+//	0 0 2 2
+//	0 0 0 2
+//  2 0 2 2
+
+	public void addLeft() {
+		for (int i = 0; i < table.length; i++)
+		for (int j = 0; j < table.length - 1; j++) {
+			if (table [i][j] == table[i][j + 1]) {
+				table[i][j] = 2 * table[i][j + 1];
+				table[i][j + 1] = 0;
+			}
+		}
+		
+	}
+	
+	//0 0 2 4
+	//0 0 2 4
+    //4 0 0 2
+	
+	
+	public void left() {
+		addLeft();
+		moveLeft();
+		
+	}
+
+	private void moveLeft() {
+
+		for (int i = 0; i < table.length; i++)
+		for (int k = 0; k < table.length -  1; k++)
+		for (int j = 0; j < table.length - 1; j++) {
+			if (table[i][j] == 0) {
+				table[i][j] = table[i][j + 1];
+				table[i][j + 1] = 0;
+			}
+		}
+	}
+	
+	private void moveRight() {
+
+		int i = 0;
+		for (int k = 0; k < table.length -  1; k++)
+		for (int j = table.length - 1; j > 0; j--) {
+			if (table[i][j] == 0) {
+				table[i][j] = table[i][j -1];
+				table[i][j - 1] = 0;
+			}
+		}
+	}
+
+	public void right() {
+		moveRight();
+	}
+
 }
+
+	
--- Game2048/src/test/java/bence/game2048/Game2048Test.java
+++ Game2048/src/test/java/bence/game2048/Game2048Test.java
@@ -1,11 +1,17 @@
 package bence.game2048;
 
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.inOrder;
 
 import java.io.PrintStream;
+import java.util.Random;
 
+import org.fusesource.jansi.AnsiConsole;
+import org.hamcrest.core.Is;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InOrder;
@@ -14,8 +20,18 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
 
+import biz.source_code.utils.RawConsoleInput;
+
 public class Game2048Test {
 	
+	private static final int FOURTH_ROW = 3;
+
+	private static final int THIRD_ROW = 2;
+
+	private static final int SECOND_ROW = 1;
+
+	private static final int FIRST_ROW = 0;
+
 	@Rule
     public MockitoRule rule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 	
@@ -31,11 +47,18 @@ public class Game2048Test {
 	private static final String CELL_LEFT = "\u2560";
 	private static final String CELL_RIGHT = "\u2563";
 	private static final String SPACE = " ";
+	private static final String ANSI_CLS = "\u001b[2J";
 	
-	int[][] content = new int[4][4];
+	private static final int LEFT = 57419;
+	private static final int RIGHT = 57421;
+	private static final int UP = 57416;
+	private static final int DOWN = 57424;
+	
+	
+	int[][] table = new int[4][4];
 	
 	@Spy
-	private PrintStream out = new PrintStream(System.out);
+	private PrintStream out = new PrintStream(AnsiConsole.out);
 	InOrder inOrder = null;
 
 	Game2048 g = new Game2048();
@@ -43,52 +66,210 @@ public class Game2048Test {
 	@Before
 	public void setup() {
 		g.out = out;
-		g.content = content;
 		inOrder = inOrder(out);
-		
 	}
+	
 
-	@Test
-	public void testName() throws Exception {
-		
-	}
 	
 	@Test
 	public void testPrintTableWithoutContent() throws Exception {
 		
 		g.printTable();
-		
-		inOrder.verify(out).print(topBorderRow());
-		inOrder.verify(out).print(contentRow(content[0]));
-		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[1]));
-		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[2]));
-		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[3]));
-		inOrder.verify(out).print(bottomBorderRow());
+		assertThatTablePrintedCorrectly();
 		
 	}
 	
 	@Test
 	public void testPrintContentOneElement() throws Exception {
-		content[0][0] = 2;
+		table[0][0] = 2;
+		g.table = table;
+
+		g.printTable();
 		
-		g.content = content;
+		assertThatTablePrintedCorrectly();
+		
+	}
+	
+	@Test
+	public void moving() throws Exception {
+		out.print(ANSI_CLS);
+		table[0][0] = 2;
+		g.table = table;
+
+		g.printTable();
+		
+//		Thread.sleep(500);  //In order to verify it visually
+		
+		assertThatTablePrintedCorrectly();
+		out.print(ANSI_CLS);
+		
+		table[0][0] = 0;
+		table[0][1] = 2;
+		g.printTable();
+		
+//		Thread.sleep(500); //In order to verify it visually
+		assertThatTablePrintedCorrectly();
+	}
+	
+	
+	
+	@Test
+	public void testPrintContentAllElements() throws Exception {
+		table[0][0] = 2;
+		table[0][1] = 4;
+		table[0][2] = 8;
+		table[0][3] = 16;
+		table[1][0] = 32;
+		table[1][1] = 64;
+		table[1][2] = 128;
+		table[1][3] = 256;
+		table[2][0] = 512;
+		table[2][1] = 1024;
+		table[2][2] = 2048;
+		table[2][3] = 1024;
+		table[3][0] = 521;
+		table[3][1] = 256;
+		table[3][2] = 128;
+		table[3][3] = 64;
+		g.table = table;
 		
 		g.printTable();
 		
+		assertThatTablePrintedCorrectly();
+		
+	}
+	
+	@Test
+	public void placeFirstCellRandomly() throws Exception {
+		
+		g.start();
+		int found = 0;
+		for (int i = 0; i < table.length; i++) {
+			for (int j = 0; j < table.length; j++) {
+				if (g.table[i][j] > 0) found++;
+			}
+			
+		}
+		assertThat(found, is(1));
+		
+		g.printTable();
+		
+		
+	}
+	
+	@Test
+	@Ignore
+	public void testReadKey() throws Exception {
+		
+		System.out.println("trying to read a character:");
+		int read = RawConsoleInput.read(true);
+		if (read == UP) System.out.println("UP was pressed");
+		if (read == DOWN) System.out.println("DOWN was pressed");
+		if (read == LEFT) System.out.println("LEFT was pressed");
+		if (read == RIGHT) System.out.println("RIGHT was pressed");
+		
+	}
+	
+	@Test
+	public void testMoveOneValueLeft() throws Exception {
+		int randomValue = 2;
+		g.table[3][3] = randomValue;
+		g.printTable();
+		
+		g.left();
+		
+		assertThat(g.table[3][0], is(randomValue));
+		g.printTable();
+	}
+	
+	@Test
+	public void testMoveValuesLeftAndAddThemIfTheyAreEqual() throws Exception {
+		int randomValue = 2;
+		g.table[3][3] = randomValue;
+		g.table[3][2] = randomValue;
+		g.printTable();
+		
+		g.left();
+		
+		assertThat(g.table[3][0], is(2 * randomValue));
+		g.printTable();
+	}
+	
+	@Test
+	public void testMoveValuesLeftAndDontAddThemIfTheyAreNotEqual() throws Exception {
+		g.table[3][3] = 4;
+		g.table[3][2] = 2;
+		g.printTable();
+		
+		g.left();
+		
+		assertThat(g.table[3][0], is(2));
+		assertThat(g.table[3][1], is(4));
+		g.printTable();
+	}
+	
+	@Test
+	public void testDontMoveValuesLeft() throws Exception {
+		g.table[3][3] = 4;
+		g.table[3][2] = 2;
+		g.table[3][1] = 4;
+		g.table[3][0] = 2;
+		g.printTable();
+		
+		g.left();
+		
+		assertThat(g.table[3][0], is(2));
+		assertThat(g.table[3][1], is(4));
+		assertThat(g.table[3][2], is(2));
+		assertThat(g.table[3][3], is(4));
+		g.printTable();
+	}
+	
+	@Test
+	public void testMoveMultipleRowsLeftAndAddValues() throws Exception {
+		g.table[3][3] = 2;
+		g.table[3][2] = 2;
+		g.table[2][3] = 2;
+		g.table[2][2] = 2;
+		
+		g.printTable();
+		
+		g.left();
+		
+		assertThat(g.table[3][0], is(4));
+		assertThat(g.table[3][1], is(0));
+		assertThat(g.table[3][2], is(0));
+		assertThat(g.table[3][3], is(0));
+		
+		assertThat(g.table[2][0], is(4));
+		assertThat(g.table[2][1], is(0));
+		assertThat(g.table[2][2], is(0));
+		assertThat(g.table[2][3], is(0));
+		g.printTable();
+	}
+	
+	@Test
+	public void testMoveOneValueRight() throws Exception {
+		int randomValue = 2;
+		g.table[0][0] = randomValue;
+		g.printTable();
+		
+		g.right();
+		
+		assertThat(g.table[0][3], is(randomValue));
+		g.printTable();
+	}
+
+	private void assertThatTablePrintedCorrectly() {
 		inOrder.verify(out).print(topBorderRow());
-		inOrder.verify(out).print(contentRow(content[0]));
+		inOrder.verify(out).print(contentRow(table[FIRST_ROW]));
 		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[1]));
+		inOrder.verify(out).print(contentRow(table[SECOND_ROW]));
 		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[2]));
+		inOrder.verify(out).print(contentRow(table[THIRD_ROW]));
 		inOrder.verify(out).print(middleBorderRow());
-		inOrder.verify(out).print(contentRow(content[3]));
+		inOrder.verify(out).print(contentRow(table[FOURTH_ROW]));
 		inOrder.verify(out).print(bottomBorderRow());
-		
-		
 	}
 
 	private String topBorderRow() {
